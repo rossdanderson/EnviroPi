92f2a21a0e276c592518027c88b96252f199dca2
Ross Anderson <ross@coroutines.co.uk>
2025-05-07 13:09:21 +0100

    Fix the bucketing

--- src/main/kotlin/uk/co/coroutines/enviropi/ui/Card.kt
+++ src/main/kotlin/uk/co/coroutines/enviropi/ui/Card.kt
@@ -2,16 +2,14 @@ package uk.co.coroutines.enviropi.ui
 
 import kotlin.time.Duration
 import kotlinx.datetime.Instant
+import org.jetbrains.kotlinx.dataframe.DataColumn
+import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.aggregate
-import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
-import org.jetbrains.kotlinx.dataframe.api.expr
 import org.jetbrains.kotlinx.dataframe.api.first
 import org.jetbrains.kotlinx.dataframe.api.groupBy
 import org.jetbrains.kotlinx.dataframe.api.last
 import org.jetbrains.kotlinx.dataframe.api.max
-import org.jetbrains.kotlinx.dataframe.api.mean
 import org.jetbrains.kotlinx.dataframe.api.min
-import org.jetbrains.kotlinx.dataframe.api.toColumn
 import org.jetbrains.kotlinx.kandy.dsl.plot
 import org.jetbrains.kotlinx.kandy.letsplot.export.toSVG
 import org.jetbrains.kotlinx.kandy.letsplot.feature.layout
@@ -35,31 +33,13 @@ data class Card(
         colour: String,
         title: String,
         unit: String,
-        accessor: (Data) -> Double
+        df: DataFrame<*>,
+        column: DataColumn<Double>
     ): Any {
-      val current = "%.2f".format(accessor(data.last()))
-      val min = "%.2f".format(data.minOf { accessor(it) })
-      val max = "%.2f".format(data.maxOf { accessor(it) })
-
-      val instants = data.map { it.instant }
-
-      val time = instants.toColumn("time")
-      val titleColumn = data.map { accessor(it) }.toColumn(title)
-      val df =
-          dataFrameOf(time, titleColumn)
-              .groupBy {
-                expr("timeKey") { get(time).roundDownTo((instants.max() - instants.max()) / 100) }
-              }
-              .aggregate {
-                get(titleColumn).first() into "open"
-                get(titleColumn).max() into "high"
-                get(titleColumn).min() into "low"
-                get(titleColumn).last() into "close"
-                get(titleColumn).mean() into "mean"
-              }
-
       val svg =
-          df.plot {
+          df.groupBy { get("timeKey") }
+              .aggregate { get(column).first() into "open" }
+              .plot {
                 line {
                   x("timeKey")
                   y("open")
@@ -74,6 +54,9 @@ data class Card(
               }
               .toSVG()
 
+      val current = "%.2f".format(df.last()[column])
+      val min = "%.2f".format(df.min(column))
+      val max = "%.2f".format(df.max(column))
       return Card(title, colour, current, min, max, unit, icon, svg)
     }
   }
--- src/main/kotlin/uk/co/coroutines/enviropi/ui/Routing.kt
+++ src/main/kotlin/uk/co/coroutines/enviropi/ui/Routing.kt
@@ -6,12 +6,33 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import kotlinx.coroutines.flow.StateFlow
+import org.jetbrains.kotlinx.dataframe.api.add
+import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
+import org.jetbrains.kotlinx.dataframe.api.toColumn
+import org.tinylog.kotlin.Logger.info
 import uk.co.coroutines.enviropi.Data
 
 fun Application.routing(state: StateFlow<List<Data>>) {
   routing {
     get("/") { call.respond(JteContent("index.kte", emptyMap())) }
     get("/table") {
+      val instants = state.value.map { it.instant }
+      val time = instants.toColumn("time")
+
+      val temperature = state.value.map { it.temperature }.toColumn("temperature")
+      val humidity = state.value.map { it.humidity }.toColumn("humidity")
+      val lux = state.value.map { it.lux }.toColumn("lux")
+      val pressure = state.value.map { it.pressure }.toColumn("pressure")
+
+      val duration = (instants.max() - instants.min()) / 100
+
+      info { "Rounding to $duration" }
+
+      val df =
+          dataFrameOf(time, temperature, humidity, lux, pressure).add {
+            "timeKey" from { get(time).roundDownTo(duration) }
+          }
+
       call.respond(
           JteContent(
               "card-grid.kte",
@@ -24,28 +45,25 @@ fun Application.routing(state: StateFlow<List<Data>>) {
                               "--wa-color-red-50",
                               "Temperature",
                               "°C",
-                              Data::temperature),
+                              df,
+                              temperature),
                           Card(
                               state.value,
                               "droplet",
                               "--wa-color-blue-60",
                               "Humidity",
                               "%",
-                              Data::humidity),
-                          Card(
-                              state.value,
-                              "sun",
-                              "--wa-color-yellow-80",
-                              "Light",
-                              "lux",
-                              Data::lux),
+                              df,
+                              humidity),
+                          Card(state.value, "sun", "--wa-color-yellow-80", "Light", "lux", df, lux),
                           Card(
                               state.value,
                               "gauge",
                               "--wa-color-green-60",
                               "Pressure",
                               "hPa",
-                              Data::pressure)))))
+                              df,
+                              pressure)))))
     }
   }
 }
